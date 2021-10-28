6d5816de19ed2a0253d8ed82b660d651ab37abdd
Teng <704279465@qq.com>
2021-10-29 00:31:23 +0800

    ref 定义响应的基础类型,reactive:定义对象类型;toRefs 将响应式对象转换为普通对象

---
+++ src/views/account/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="account">
+    <div class="form-wrap">
+      <a-form name="custom-validation" ref="formRef" v-bind="formConfig.layout">
+        <a-form-item has-feedback label="用户名" name="pass">
+          <a-input type="text" autocomplete="off"/>
+        </a-form-item>
+
+
+        <a-form-item has-feedback label="密码" name="pass">
+          <a-input type="password" autocomplete="off" />
+        </a-form-item>
+
+        <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+          <a-button type="primary" html-type="submit" block>登录</a-button>
+        </a-form-item>
+      </a-form>
+      <div class="text-center fs-12">
+        <a class="color-white" >忘记密码</a> |
+        <a class="color-white" href="">注册</a>
+      {{count}}
+
+      <a-button @click="set()">按钮</a-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {},
+  setup(props) {
+
+    let count = ref<string>("10");
+
+    console.log(count.value);
+
+    const formConfig = reactive({
+      layout: {
+        labelCol: {
+          span: 4,
+        },
+        wrapperCol: {
+          span: 14,
+        },
+      },
+    });
+    const data = toRefs(formConfig);
+
+    const set = () => {
+      count.value= count.value+"20"+1;
+    }
+    console.log(data);
+    return {  
+      formConfig,count,set
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+@import "./style.scss";
+</style>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+module.exports={
+    // eslint-disable-next-line prettier/prettier
+    publicPath://
+        process.env.NODE_ENV === 'production' ? '': '/',
+    outputDir: //构建项目生成的目录
+        process.env.NODE_ENV === 'production'? 'dist': 'dev-dist',
+    lintOnSave: //关闭编译时候的警告
+        false,
+        css: {
+            loaderOptions: {             
+              scss: {
+                prependData: `@import "./src/styles/main.scss";`
+              }
+            }
+          }
+}
