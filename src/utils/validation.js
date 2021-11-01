d0491815340b2a191c438e7dc2e6b5b362f66391
tenglingfeng <704279465@qq.com>
2021-11-01 23:17:33 +0800

    crlf

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,20 +1,19 @@
-/* eslint-disable prettier/prettier */
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 /**
- * 
- * @param {手机号}} value 
- * @returns 
+ *
+ * @param {手机号}} value
+ * @returns
  */
-export function checkPhone(value){
-    let regPhone =/^1[3456789]\d{9}$/;
-    return regPhone.test(value);
+export function checkPhone(value) {
+  let regPhone = /^1[3456789]\d{9}$/;
+  return regPhone.test(value);
 }
 
 /**
- * 
- * @param {密码} value 
- * @returns 
+ *
+ * @param {密码} value
+ * @returns
  */
-export function checkPassword(value){
-    return true;
+export function checkPassword() {
+  return true;
 }
+
