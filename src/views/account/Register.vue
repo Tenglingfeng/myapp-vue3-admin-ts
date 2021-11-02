15d18a4249a8e480e29f5577228b3acb06448e7d
tenglingfeng <704279465@qq.com>
2021-11-02 23:30:40 +0800

    1

--- src/views/account/Register.vue
+++ src/views/account/Register.vue
@@ -37,11 +37,20 @@
               <a-input
                 type="text"
                 autocomplete="off"
+                maxLength="6"
                 v-model:value="account_form.code"
               />
             </a-col>
             <a-col :span="10">
-              <a-button type="primary" block>获取验证码</a-button>
+              <a-button
+                type="primary"
+                block
+                @click="getCode"
+                :loading="btn_loading"
+                :disabled="btn_disabled"
+              >
+                {{ button_text }}
+              </a-button>
             </a-col>
           </a-row>
         </a-form-item>
@@ -54,21 +63,24 @@
 </template>
 
 <script lang="ts">
-//import { checkPhone } from "@/utils/validation";
+import { checkPhone } from "@/utils/validation";
+import { message } from "ant-design-vue";
 import { RuleObject } from "ant-design-vue/lib/form/interface";
 import { defineComponent, reactive, toRefs } from "vue";
 //局部组件
 export default defineComponent({
   name: "register",
   components: {},
-  setup(props) {
-    let validateUserName = async (_rule: RuleObject, value: string) => {
-      // if (!checkPhone(value)) {
-      //   console.log(value);
-      //   return Promise.reject("请输入正确的手机号");
-      // }
+  setup() {
+    async function validateUserName(
+      _rule: RuleObject,
+      value: string
+    ): Promise<void> {
+      if (!checkPhone(value)) {
+        return Promise.reject("请输入正确的手机号");
+      }
       return Promise.resolve();
-    };
+    }
 
     const formConfig = reactive({
       layout: {
@@ -101,23 +113,54 @@ export default defineComponent({
         ],
         password2: [
           {
-            //validator: validatePass,
             trigger: "change",
             required: true,
           },
         ],
       },
     });
-    const data = toRefs(formConfig);
 
-    //表单验证
-    const handleFinish = () => {
-      alert(111);
+    const dataItem = reactive({
+      button_text: "获取验证码",
+      btn_loading: false,
+      btn_disabled: false,
+      sec: 10,
+      //定时器
+      timer: 0,
+    });
+    const form = toRefs(formConfig);
+    const data = toRefs(dataItem);
+    //表单提交
+    const handleFinish = (value: unknown) => {
+      console.log(value);
     };
+    //获取验证码
+    function getCode(): void {
+      //用户名不存在的情况
+      if (!formConfig.account_form.username) {
+        message.warning({
+          content: "用户名不能为空",
+        });
+      }
+
+      //先判断定时器是否存在, 存在则先清除
+      dataItem.timer && clearInterval(dataItem.timer);
+      dataItem.timer = setInterval(() => {
+        let s = dataItem.sec--;
+        console.log(s);
+        dataItem.button_text = `${s}秒`;
+        if (s <= 0) {
+          clearInterval(dataItem.timer);
+          dataItem.button_text = "重新获取";
+        }
+      }, 1000);
+    }
 
     return {
+      ...form,
       ...data,
       handleFinish,
+      getCode,
     };
   },
 });
